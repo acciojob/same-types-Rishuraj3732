9efa5f54126d28836b32d1e18f3d781497b128b6
Rishuraj3732 <103851611+Rishuraj3732@users.noreply.github.com>
2025-08-05 01:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 function isSameType(value1, value2) {
-  try {
-    const parsed1 = JSON.parse(value1);
-    const parsed2 = JSON.parse(value2);
-    return typeof parsed1 === typeof parsed2;
-  } catch {
-    // fallback if input can't be parsed (e.g., user enters a word)
-    return typeof value1 === typeof value2;
-  }
+  if (Number.isNaN(value1) || Number.isNaN(value2)) return false;
+  return typeof value1 === typeof value2;
 }
 
 let value1 = prompt("Enter Start of the Range.");
