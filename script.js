991331de68cabab2c1ff09adfeb6afeb9d7a0ef3
Rishuraj3732 <103851611+Rishuraj3732@users.noreply.github.com>
2025-08-05 01:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function isSameType(value1, value2) {
   //your js code here
-	return typeof(value1)===typeof(value2);
+	try {
+    const val1 = JSON.parse(value1);
+    const val2 = JSON.parse(value2);
+    return typeof val1 === typeof val2;
+  } catch {
+    // If JSON.parse fails, treat both as strings
+    return true; // both are strings if parse fails
+  }
 }
 
 // do not change the code below.
